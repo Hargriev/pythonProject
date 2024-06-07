621fa9312ef665ecacdb5d784762a94c19f2a823
Vladislav Fedyaev <Hargie@mail.ru>
2024-06-07 18:58:58 +0500

    гunion defs in widjet (almost works)

--- src/widget.py
+++ src/widget.py
@@ -1,11 +1,18 @@
-def mask_account_card(number: str) -> str | None:
+def mask_account_card(numbers: str) -> str | None:
     """Функция, возвращающая маску карты"""
-    if number.isdigit() and len(number) == 16:
-        return f"{number[:4]} {number[5:7]}{"*" * 2} {"*" * 4} {number[12:]}"
-    elif number.isdigit() and len(number) == 20:
-        return f"{'*' * 2}{number[-4::]}"
+    name_var = ''
+    numbers_var = ''
+    for number in numbers:
+        if number.isalpha():
+            name_var += number
+        if number.isdigit():
+            numbers_var += number
+    if numbers_var.isdigit() and len(numbers_var) == 16:
+        return f"{name_var} {numbers_var[:4]} {numbers_var[5:7]}{"*" * 2} {"*" * 4} {numbers_var[12:]}"
+    elif numbers_var.isdigit() and len(numbers_var) == 20:
+        return f"{name_var} {'*' * 2}{numbers_var[-4::]}"
     else:
         return None
 
 
-print(mask_account_card('Maestro 1596837868705199'))
+print(mask_account_card('Visa Platinum 7000 7922 8960 6361'))
