95f791800924ed97afa04093f5674258ed20a856
Vladislav Fedyaev <Hargie@mail.ru>
2024-06-26 18:52:21 +0500

    fix func filter_by_state and start writing func sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,7 @@
 from typing import Any
 
 
-def filter_by_state(list_dicts: list[dict[str, Any]], state='EXECUTED') -> Any:
+def filter_by_state(list_dicts: list[dict[str, Any]], state: str = 'EXECUTED') -> list[dict[str, Any]]:
     """Функция фильтрации словарей по значению ключа state"""
     filtered_list = []
     for key in list_dicts:
@@ -16,15 +16,16 @@ print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03
                        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
 
 
-def sort_by_date():
+def sort_by_date(list_dicts: list[dict[str, Any]], reverse ):
     pass
 
 
-# # Вход функции
-# [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-#  {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-#  {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-#  {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+print(sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+                    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}])
+
+
 # # Выход функции (сортировка по убыванию, т. е. сначала самые последние операции)
 # [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
 # {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
