ca521cd6812eff9b65febbb998fdc2eab0a57989
Vladislav Fedyaev <Hargie@mail.ru>
2024-06-26 18:36:43 +0500

    func filter_by_state ready

--- src/processing.py
+++ src/processing.py
@@ -1,18 +1,19 @@
-def filter_by_state():
-    pass
+from typing import Any
 
 
-# # Вход функции
-# [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-#  {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-#  {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-#  {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
-# # Выход функции со статусом по умолчанию EXECUTED
-# [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-# {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
-# # Выход функции, если вторым аргументов передано 'CANCELED'
-# [{'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-# {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+def filter_by_state(list_dicts: list[dict[str, Any]], state='EXECUTED') -> Any:
+    """Функция фильтрации словарей по значению ключа state"""
+    filtered_list = []
+    for key in list_dicts:
+        if key.get('state') == state:
+            filtered_list.append(key)
+    return filtered_list
+
+
+print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                       {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+                       {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                       {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
 
 
 def sort_by_date():
