02239d5f61321c4be290f29ddf83cbfa5b4241db
Vladislav Fedyaev <Hargie@mail.ru>
2024-06-28 15:30:05 +0500

    fix masks.py

--- src/masks.py
+++ src/masks.py
@@ -1,4 +1,4 @@
-def get_mask_card_number(card_number: str) -> str | None:
+def get_mask_card_number(card_number: str) -> str:
     """Функция, возвращающая маску карты"""
     if card_number.isdigit() and len(card_number) == 16:
         return f"{card_number[:4]} {card_number[4:6]}{"*" * 2} {"*" * 4} {card_number[12:]}"
@@ -9,7 +9,7 @@ def get_mask_card_number(card_number: str) -> str | None:
 print(get_mask_card_number("7000792289606361"))
 
 
-def get_mask_account(account_number: str) -> str | None:
+def get_mask_account(account_number: str) -> str:
     """Функция, возвращающая маску счета"""
     if account_number.isdigit() and len(account_number) == 20:
         return f"{'*' * 2}{account_number[-4::]}"
