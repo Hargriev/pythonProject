d6dad1ffbb3f2362b7efe82ffacff5ca660c4a72
Vladislav Fedyaev <Hargie@mail.ru>
2024-06-28 22:38:31 +0500

    tests ready, flake8, black, other fixes

--- src/generators.py
+++ src/generators.py
@@ -1,13 +1,13 @@
 transactions = [
     {
         "id": 939719570,
-        "state": "EXECUTED",  # состояние, статус
-        "date": "2018-06-30T02:08:58.425572",  # дата
-        "operationAmount": {  # сумма операции
-            "amount": "9824.07",  # количество, сумма
-            "currency": {"name": "USD", "code": "USD"},  # валюта  # имя  # код
+        "state": "EXECUTED",
+        "date": "2018-06-30T02:08:58.425572",
+        "operationAmount": {
+            "amount": "9824.07",
+            "currency": {"name": "USD", "code": "USD"},
         },
-        "description": "Перевод организации",  #
+        "description": "Перевод организации",
         "from": "Счет 75106830613657916952",
         "to": "Счет 11776614605963066702",
     },
--- tests/test_generators.py
+++ tests/test_generators.py
@@ -2,3 +2,66 @@ import pytest
 from src.generators import filter_by_currency, transaction_descriptions, card_number_generator
 
 
+@pytest.fixture
+def test_filter_by_currency(transactions, currency):
+    assert filter_by_currency(transactions, "RUB") == [
+        {
+            "id": 873106923,
+            "state": "EXECUTED",
+            "date": "2019-03-23T01:09:46.296404",
+            "operationAmount": {
+                "amount": "43318.34",
+                "currency": {"name": "руб.", "code": "RUB"},
+            },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 44812258784861134719",
+            "to": "Счет 74489636417521191160",
+        },
+        {
+            "id": 594226727,
+            "state": "CANCELED",
+            "date": "2018-09-12T21:27:25.241689",
+            "operationAmount": {
+                "amount": "67314.70",
+                "currency": {"name": "руб.", "code": "RUB"},
+            },
+            "description": "Перевод организации",
+            "from": "Visa Platinum 1246377376343588",
+            "to": "Счет 14211924144426031657",
+        },
+    ]
+
+
+def test_transaction_descriptions():
+    transactions = [
+        {"description": "Перевод организации"},
+        {"description": "Перевод со счета на счет"},
+        {"description": "Перевод со счета на счет"},
+        {"description": "Перевод с карты на карту"},
+        {"description": "Перевод организации"},
+    ]
+
+    descriptions = list(transaction_descriptions(transactions))
+    expected = [
+        "Перевод организации",
+        "Перевод со счета на счет",
+        "Перевод со счета на счет",
+        "Перевод с карты на карту",
+        "Перевод организации",
+    ]
+
+    assert descriptions == expected
+
+
+def test_card_number_generator():
+    generator = card_number_generator(1, 10)
+    assert next(generator) == "0000 0000 0000 0001"
+    assert next(generator) == "0000 0000 0000 0002"
+    assert next(generator) == "0000 0000 0000 0003"
+    assert next(generator) == "0000 0000 0000 0004"
+    assert next(generator) == "0000 0000 0000 0005"
+    assert next(generator) == "0000 0000 0000 0006"
+    assert next(generator) == "0000 0000 0000 0007"
+    assert next(generator) == "0000 0000 0000 0008"
+    assert next(generator) == "0000 0000 0000 0009"
+    assert next(generator) == "0000 0000 0000 0010"
